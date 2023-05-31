7ba58cf51bafb6b43d58ba774cb4fd87b7bd1f31
Hudson Souza <hudsonsoouzati@gmail.com>
2023-05-31 00:48:13 -0300

    Scroll Animation

--- script.js
+++ script.js
@@ -62,3 +62,27 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function animacaoInit() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach(function (section) {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        // console.log(sectionTop);
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+animacaoInit();
