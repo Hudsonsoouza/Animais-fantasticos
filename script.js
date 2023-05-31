38cec92040b6ff6bda1b6437dd81b2ae41709c07
Hudson Souza <hudsonsoouzati@gmail.com>
2023-05-31 00:08:44 -0300

    Scroll Suave

--- script.js
+++ script.js
@@ -37,3 +37,28 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    // const topo = section.offsetTop;
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    //   window.scrollTo({
+    //     top: topo,
+    //     behavior: "smooth",
+    //   });
+  }
+
+  linksInternos.forEach(function (link) {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
